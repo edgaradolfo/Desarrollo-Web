c2b3208666aefafd6b424914ac921e75ed6c9175
edgaradolfo <40681838+edgaradolfo@users.noreply.github.com>
2019-10-02 19:33:03 -0600

    Add files via upload

--- public/js/funcionalidad.js
+++ public/js/funcionalidad.js
@@ -88,8 +88,7 @@ function mostrarformularioOtros(){
                 <img src="img/subirarchivosgenericos3.png" style="margin-left: 90px;" alt="">
             </div>
         </div>
-        <hr>
-        <div class="col-12">
+        <hr>        <div class="col-12">
             <input type="text" class="form-control" placeholder="Nombre Archivo">
         </div>
         <hr>
@@ -119,11 +118,74 @@ function mostrarformularioOtros(){
     document.getElementById('formularioArchivo').style.display = 'block';
 }
 
+function mostrarformularioComentario(){
+    document.getElementById('formularioComentario').style.display = 'none';
+    document.getElementById('formularioComentario').innerHTML = '';
+    document.getElementById('formularioComentario').innerHTML += `
+    <div class="card-body">
+            <div class="card" style="border-color:rgba(243, 53, 28, 0.856);">
+                <div class="container">
+                    <label for="" style="font-size: 19px; text-shadow: 2px 2px 8px #FF0000; margin-top: 19px;">Comentar</label>
+                    <textarea name="" class="form-control" id="" cols="30" rows="5" style="margin-bottom: 19px; margin-top: 5px;"></textarea>
+                    <div class="row">
+                        <div class="col-3">
+                            <button type="button" id="botonArchivos" style="margin-bottom: 19px !important; margin-top: 0px !important; margin-left: 0px;">Ingresar Comentario</button>
+                        </div>
+                        <div class="col-3">
+                            <button type="button" onclick="esconderCajaComentario()" id="botonArchivos" style="margin-bottom: 19px !important; margin-top: 0px !important; margin-left: 0px;">Cancelar Comentario</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+    `;
+    document.getElementById('formularioComentario').style.display = 'block';
+    window.scrollBy(0, 350);
+}
+
+function mostrarformularioVerComentario(){
+    document.getElementById('formularioVerComentarios').style.display = 'none';
+    document.getElementById('formularioVerComentarios').innerHTML = '';
+    document.getElementById('formularioVerComentarios').innerHTML += `
+    <div class="card-body">
+        <div class="card" style="border-color:rgba(243, 53, 28, 0.856);">
+            <div class="container d-table">
+                <div class="row">
+                    <div class="col-3 text-right" style="margin-top: 19px;">
+                        <p>Usuario :</p>
+                    </div>
+                    <div class="col-8 text-left" style="margin-top: 19px;">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, expedita deserunt. Cumque, quos quas, magnam eligendi consequatur vel tenetur voluptatem tempore minima ex facilis quaerat sit consequuntur odit velit rerum.</p>
+                    </div>
+                    <div class="col-12">
+                        <hr style="background-color: rgba(243, 53, 28, 0.856);">
+                    </div>
+                    <div class="col-3 text-right" style="margin-top: 19px;">
+                        <p>Usuario2 :</p>
+                    </div>
+                    <div class="col-8 text-left" style="margin-top: 19px;">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, expedita deserunt. Cumque, quos quas, magnam eligendi consequatur vel tenetur voluptatem tempore minima ex facilis quaerat sit consequuntur odit velit rerum.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    document.getElementById('formularioVerComentarios').style.display = 'block';
+    window.scrollBy(0, 350);
+}
+
 function esconder(){
     document.getElementById('formularioArchivo').style.display = 'none';
     document.getElementById('formularioArchivo').innerHTML = '';
 }
 
+function esconderCajaComentario(){
+    document.getElementById('formularioComentario').style.display = 'none';
+    document.getElementById('formularioComentario').innerHTML = '';
+}
+
 function mostrarOtrasOpciones(){
     document.getElementById('div-opciones').style.display='block'
 }
+
