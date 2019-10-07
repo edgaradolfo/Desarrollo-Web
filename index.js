0f8dcbb285015b98a33fcf309d41b8add8e4d08a
edgaradolfo <40681838+edgaradolfo@users.noreply.github.com>
2019-10-06 20:20:06 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var database = require("./modules/database");
+var bodyParser = require('body-parser');
+var usuariosRouter = require("./routers/usuarios-router");
 var app = express();
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 //Exponer una carpeta como publica para archivos estaticos
 app.use(express.static("public"));
+app.use('/usuarios',usuariosRouter);
 
 app.listen(8888, function(){
     console.log("Servidor levantado");
