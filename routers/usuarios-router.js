378a92cf9cf9335ae9f6f43ca5266b9ae254ab53
edgaradolfo <40681838+edgaradolfo@users.noreply.github.com>
2019-10-06 20:19:27 -0600

    Add files via upload

---
+++ routers/usuarios-router.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var usuario = require('../models/usuario');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+router.get('/',function(req,res){
+    usuario.find()
+    .then((data)=>{
+        res.send(data);
+        res.end();
+    })
+    .catch((error)=>{
+        res.send(error);
+        res.end();
+    });
+});
+
+router.post('/',function(req,res){
+    let u = new usuario({
+        nombre:req.body.nombre,
+        correo:req.body.correo,
+        contrasena:req.body.contrasena,
+        observacion:req.body.observacion,
+        tipoUsuario:req.body.tipoUsuario,
+        posts:[]
+    }); 
+
+    //Promesa
+    u.save()
+    .then(function(obj){
+        res.send(obj);
+        res.end();
+    })
+    .catch(function(error){
+        res.send(error);
+        res.end();
+    });
+});
+
+router.delete('/:id',function(req,res){
+    usuario.remove({_id:req.params.id})
+    .then((result)=>{
+        res.send(result);
+        res.end();
+    })
+    .catch((error)=>{
+        res.send(error);
+        res.end();
+    });
+});
+
+
+
+module.exports = router;
+
